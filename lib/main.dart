cb54cc0a6615edae6a8379c67bc543b0d9b86e12
shivam3009 <shivamrajsingh3009@gmail.com>
2021-05-18 15:10:34 +0530

    stylings

--- lib/main.dart
+++ lib/main.dart
@@ -59,19 +59,34 @@ class MyHomePage extends StatelessWidget {
                       ),
                       decoration: BoxDecoration(
                           border: Border.all(
-                        color: Colors.black,
+                        color: Colors.purple,
                         width: 2,
                       )),
                       padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tx.title),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         Text(
                           tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
                         )
                       ],
                     ),
