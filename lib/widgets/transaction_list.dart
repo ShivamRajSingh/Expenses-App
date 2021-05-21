41af52631baceffb46aacf2a3c6c7b85bbd3e6ae
shivam3009 <shivamrajsingh3009@gmail.com>
2021-05-21 17:12:05 +0530

    Scrollings added

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,59 +5,64 @@ import '../models/transaction.dart';
 import './user_transactions.dart';
 
 class TransactionList extends StatelessWidget {
-  List <Transaction> transactions;
+  List<Transaction> transactions;
 
   TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Rs.${tx.amount}',
-                  style: TextStyle(
-                    color: Colors.purple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 600,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Rs.${tx.amount}',
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat.yMMMMd().format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  )
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMMd().format(tx.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
