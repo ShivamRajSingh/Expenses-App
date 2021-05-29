188020504cb2d009600259e9a8927d16a3ab9cfe
shivam3009 <shivamrajsingh3009@gmail.com>
2021-05-29 12:51:53 +0530

    ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               width: double.infinity,
               child: Card(
-                color: Colors.yellow,
+                color: Colors.yellow, 
                 child: Chart(_userTransactions),
                 elevation: 5,
               ),
